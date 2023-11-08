153fe93726574fc54fa3b7ba084a2349f70c2ebc
Gabriel Aguilar <ubiquitousgabriel@gmail.com>
2023-11-08 18:57:32 -0500

    +gimmymynumber

---
+++ gimmymynumber.py
@@ -0,0 +1,11 @@
+compNum = 100
+userNum = int(input("Gimmy a number greater than 100 plz…"))
+
+while userNum <= 100:
+    print(userNum , " is less than 100, dummy. Try again... I've got all day...")
+    userNum = int(input("Gimmy a number greater than 100 plz…"))
+
+
+print(userNum , " is greater than 100! Good work.")
+
+  
